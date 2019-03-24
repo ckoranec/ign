4c5fe934839a83a448092b1caff0f02076fb57fc
ckoranec <jkertgat@e1z1r7p16.42.us.org>
2019-03-24 03:58:48 -0700

    .

---
+++ client.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+void error(const char *error)
+{
+    perror(error);
+    exit(1);
+}
+
+int main(int ac, char **av)
+{
+    int sockfd, portno, n, i;
+    char buffer[255];
+
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    if(ac < 3)
+    {
+        fprintf(stderr,"useage %s hostname port\n", av[0]);
+        exit(1);
+    }
+    
+	portno = atoi(av[2]);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0)
+        error("error opening socket");
+
+	server = gethostbyname(av[1]);
+	if(server == NULL)
+		fprintf(stderr, " error no host yo");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(portno);
+	if(connect(sockfd, ( struct sockaddr *) &serv_addr, sizeof(serv_addr))<0)
+		error("connection failed");
+	while(1)
+	{
+		bzero(buffer, 255);
+		fgets(buffer, 255, stdin);
+		n = write(sockfd, buffer, strlen(buffer));
+		if(n < 0)
+			error("error ono writing");
+
+		bzero(buffer, 255);
+		n = read(sockfd, buffer, 255);
+		if(n < 0)
+			error("error on read");
+		printf("server: %s", buffer);
+
+		i = strncmp("bye", buffer, 3);
+		if(i == 0)
+			break;
+	}
+	close(sockfd);
+	return(0);
+}
---
+++ server.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void error(const char *error)
+{
+	perror(error);
+	exit(1);
+}
+
+int main(int ac, char **av)
+{
+	int sockfd, newsockfd, portno, n, i;
+	char buff[255];
+
+	struct sockaddr_in serv_addr, cli_addr;
+	socklen_t clilen;//32 bit
+
+	if(ac < 2)
+	{
+		fprintf(stderr,"provide port plox");
+		exit(1);
+	}
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0)
+		error("error opening socket");
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = atoi(av[1]);
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+
+	if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+		error("binding failed");
+
+	listen(sockfd, 2);
+	clilen = sizeof(cli_addr);
+
+	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+	if(newsockfd < 0)
+		error("not accepted");
+
+	while(1)
+	{
+		bzero(buff, 25);
+		n = read(newsockfd, buff, 255);
+		printf("client: %s\n", buff);
+		bzero(buff, 25);
+		fgets(buff, 255, stdin);
+
+		n = write(newsockfd, buff, strlen(buff));
+		if(n < 0)
+			error("did not write");
+		i = strncmp("bye", buff, 3);
+		if(i == 0)
+			break;
+	}
+
+	close(newsockfd);
+	close(sockfd);
+	return(0);
+}
